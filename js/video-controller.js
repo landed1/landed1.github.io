e695c139007aa5b736db326ed9ad8b8a9a1b7f9e
calvin <calvincrane@gmail.com>
2014-01-15 10:07:03 +0000

    paths modified and various changes original commit in short was an old version

--- js/video-controller.js
+++ js/video-controller.js
@@ -1,1 +1,30 @@
-video-controller.js
+function VideoCtrl($scope){
+
+	//$scope.header="Some poor Call Of Duty (COD) gameplay.";
+	//$scope.blurb="I find it weird how I can watch gameplay of others and in fact I find myself observing what I did during gameplay. Spotting areas of stupidity for example rushing at tanks with small arms.";
+
+	$scope.header="I don't have time for Battlefield 4";
+	$scope.blurb="I wish I did !";
+	$scope.vidno="1";
+
+
+
+//need to swap the video in depending on the video id given in the router...
+
+	$scope.swapVideo=function($vid,$vidno,$header,$blurb){
+		//alert("swap which video");
+		//can I access the video player ?
+		//alert(window.player);
+		//console.log("loading"+riDUCw4BuMQ);
+		var myPlayer=window.player;
+		myPlayer.loadVideoById({'videoId': $vid});
+		myPlayer.playVideo();
+
+		//swap the text in.
+		$scope.vidno=$vidno;
+		$scope.header=$header;
+		$scope.blurb=$blurb;
+
+	};
+
+}
