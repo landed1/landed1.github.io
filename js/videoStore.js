17f6fc2bf524433e2e3d1da097fc4aa873233a83
calvin <calvincrane@gmail.com>
2014-01-20 15:41:27 +0000

    removed old file

--- js/videoStore.js
+++
@@ -1,5 +0,0 @@
-[
-		{id:"0",vId:"OcCK9HkZWCg",niceNumber:2,title:"Giovanni and COD",blurb:"My very first gamer dj video - hope you like the track and that you want to share."},
-		{id:"1"},
-		{id:"2"}
-]
